a85f657a1442c531ea33a3fa6f32c737e91aa57a
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-03-25 18:20:15 +0000

    added first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateSalary() {
+  // input
+  const numberOfHour = parseInt(document.getElementById('number-of-hour').value)
+  const payRate = parseInt(document.getElementById('pay-rate').value)
+  const TAX_RATE = 0.18
+
+  // process
+  const payBeforeTaxes = numberOfHour * payRate
+  const taxes = payBeforeTaxes * TAX_RATE
+  const payAfterTaxes= payBeforeTaxes - taxes
+  // output
+  document.getElementById('salary').innerHTML = "Your pay will be: $" + payAfterTaxes.toFixed(2)
+  document.getElementById('taxes').innerHTML = "Your govermnet will take will be: $" + taxes.toFixed(2)
+}
